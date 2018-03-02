1e2859019c196edf44df2ac1e2b6d711cda3226d
Johannes Donath <johannesd@torchmind.com>
2018-03-02 11:16:28 +0100

    Ported standard MCP inner type mapping.

--- mapping-mcp/src/main/java/org/basinmc/plunger/mapping/mcp/InnerClassMappingBytecodeTransformer.java
+++ mapping-mcp/src/main/java/org/basinmc/plunger/mapping/mcp/InnerClassMappingBytecodeTransformer.java
@@ -18,10 +18,17 @@ package org.basinmc.plunger.mapping.mcp;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.basinmc.plunger.bytecode.transformer.BytecodeTransformer;
+import org.basinmc.plunger.mapping.mcp.InnerClassMapping.InnerClass;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 /**
  * Provides a bytecode transformer which restores the inner class relationship using a JSON based
@@ -48,8 +55,32 @@ public class InnerClassMappingBytecodeTransformer implements BytecodeTransformer
 
   private final class InnerClassMappingClassVisitor extends ClassVisitor {
 
+    private String className;
+    private boolean visitedOuterType;
+
+    private final Set<String> visitedInnerTypes = new HashSet<>();
+    private final Set<String> referencedInnerTypes = new HashSet<>();
+
     private InnerClassMappingClassVisitor(@NonNull ClassVisitor classVisitor) {
       super(Opcodes.ASM6, classVisitor);
+    }
+
+    private boolean isInnerType(@NonNull String name) {
+      return name.contains("$");
+    }
+
+    private void reference(@NonNull Type type) {
+      if (type.getSort() == Type.ARRAY) {
+        type = type.getElementType();
+      }
+
+      if (type.getSort() == Type.OBJECT) {
+        String name = type.getInternalName();
+
+        if (this.isInnerType(name)) {
+          this.referencedInnerTypes.add(name);
+        }
+      }
     }
 
     /**
@@ -58,19 +89,121 @@ public class InnerClassMappingBytecodeTransformer implements BytecodeTransformer
     @Override
     public void visit(int version, int access, String name, String signature, String superName,
         String[] interfaces) {
-      super.visit(version, access, name, signature, superName, interfaces);
+      this.className = name;
 
-      InnerClassMappingBytecodeTransformer.this.mapping.getMapping(name)
+      Stream.of(interfaces)
+          .filter(this::isInnerType)
+          .forEach(this.referencedInnerTypes::add);
+
+      super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void visitEnd() {
+      InnerClassMappingBytecodeTransformer.this.mapping.getMapping(this.className)
           .ifPresent((e) -> {
             e.getEnclosingMethod()
+                .filter((enclosure) ->
+                    !this.visitedOuterType && enclosure.getName() != null
+                        && enclosure.getDescriptor() != null)
                 .ifPresent(
-                    (enclosure) -> this.visitOuterClass(enclosure.getOwner(), enclosure.getName(),
+                    (enclosure) -> super.visitOuterClass(enclosure.getOwner(), enclosure.getName(),
                         enclosure.getDescriptor()));
 
-            e.getInnerClasses().forEach((cl) -> this
-                .visitInnerClass(cl.getInnerClass(), cl.getOuterClass(), cl.getInnerName(),
-                    cl.getAccess()));
+            e.getInnerClasses().stream()
+                .filter((cl) -> !this.visitedInnerTypes.contains(cl.getInnerClass()))
+                .forEach((cl) -> {
+                  this.visitedInnerTypes.add(cl.getInnerClass());
+                  super.visitInnerClass(cl.getInnerClass(), cl.getOuterClass(), cl.getInnerName(),
+                      cl.getAccess());
+                });
           });
+
+      this.referencedInnerTypes.removeAll(this.visitedInnerTypes);
+
+      this.referencedInnerTypes.forEach((cl) -> {
+        InnerClass innerClass = InnerClassMappingBytecodeTransformer.this.mapping.getInnerClass(cl)
+            .orElse(null);
+
+        if (innerClass == null) {
+          int lastInnerSeparator = cl.lastIndexOf('$');
+
+          String outerName = cl.substring(0, lastInnerSeparator);
+          String innerName = cl.substring(lastInnerSeparator + 1);
+
+          super.visitInnerClass(cl, outerName, innerName, Opcodes.ACC_PUBLIC);
+          return;
+        }
+
+        super.visitInnerClass(innerClass.getInnerClass(), innerClass.getOuterClass(),
+            innerClass.getInnerName(), innerClass.getAccess());
+      });
+
+      super.visitEnd();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature,
+        Object value) {
+      this.reference(Type.getType(descriptor));
+
+      return super.visitField(access, name, descriptor, signature, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void visitInnerClass(String name, String outerName, String innerName, int access) {
+      this.visitedInnerTypes.add(name);
+      super.visitInnerClass(name, outerName, innerName, access);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
+        String[] exceptions) {
+      if (exceptions != null) {
+        Stream.of(exceptions)
+            .filter(this::isInnerType)
+            .forEach(this.referencedInnerTypes::add);
+      }
+
+      this.reference(Type.getType(descriptor));
+
+      for (Type parameterType : Type.getArgumentTypes(descriptor)) {
+        this.reference(parameterType);
+      }
+
+      MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
+
+      return new MethodVisitor(Opcodes.ASM6, visitor) {
+        @Override
+        public void visitLdcInsn(Object value) {
+          if (value instanceof Type) {
+            InnerClassMappingClassVisitor.this.reference((Type) value);
+          }
+
+          super.visitLdcInsn(value);
+        }
+      };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void visitOuterClass(String owner, String name, String descriptor) {
+      this.visitedOuterType = true;
+      super.visitOuterClass(owner, name, descriptor);
     }
   }
 }
